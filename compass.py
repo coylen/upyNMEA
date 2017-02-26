7a0a69b0de083ab5477cedd61ae22fe7d0b3744f
coylen <neil@netaccess.me.uk>
2017-02-26 20:42:38 +0000

    Updated to incorporate compass orientation issues from on board version removed unneccessary files refactored files to remove unneeded code

--- compass.py
+++ compass.py
@@ -6,41 +6,29 @@ import pyb
 from mpu9250 import MPU9250
 from fusion import Fusion
 from nmeagenerator import HDG, CMP, ERR, XDR
-import LSM303
 import pickle
 i2c_object = None
 
+
 class TCCompass:
 
-    def __init__(self, imu, hmc_side=1, timeout=1000):
+    def __init__(self, imu, timeout=1000):
         # load configuration file
         with open('calibration.conf', mode='r') as f:
             mpu_conf = f.readline()
-            lsm_conf = f.readline()
             mcnf = pickle.loads(mpu_conf)
-            lcnf = pickle.loads(lsm_conf)
         self.MPU_Centre = mcnf[0][0]
         self.MPU_TR = mcnf[1]
-        self.LSM_Centre = lcnf[0][0]
-        self.LSM_TR = lcnf[1]
         self.counter = pyb.millis()
         self.timeout = timeout
 
-        # setup compasses
-
-        # MPU9250
+        # setup MPU9250
         self.imu = imu
-
-        # LSM303
-        self.lsm = LSM303.TiltCompCompass(hmc_side)
-        self.lsm.setDeclination(0)
-        self.lsm.setContinuousMode()
 
         self.gyrobias = (-1.394046, 1.743511, 0.4735878)
 
         # setup fusions
         self.fuse = Fusion()
-        self.fuseh = Fusion()#TODO: temp for comparison
         self.update()
         self.hrp = [self.fuse.heading, self.fuse.roll, self.fuse.pitch]
 
@@ -55,15 +43,9 @@ class TCCompass:
     def update(self):
         accel = self.imu.accel.xyz
         gyroraw = self.imu.gyro.xyz
-
         gyro = [gyroraw[0] - self.gyrobias[0], gyroraw[1] - self.gyrobias[1], gyroraw[2] - self.gyrobias[2]]
-
         self.mag = self.imu.mag.xyz
-        self.magh = self.lsm.getAxes()
-
-
         self.fuse.update(accel, gyro, self.adjust_mag(self.mag, self.MPU_Centre, self.MPU_TR))
-        self.fuseh.update(accel, gyro, self.adjust_mag(self.magh, self.LSM_Centre, self.LSM_TR))
 
     def getmag(self):
         return self.imu.mag.xyz
@@ -77,7 +59,7 @@ class TCCompass:
         mx = mx_raw * TR[0][0] + my_raw * TR[0][1] + mz_raw * TR[0][2]
         my = mx_raw * TR[1][0] + my_raw * TR[1][1] + mz_raw * TR[1][2]
         mz = mx_raw * TR[2][0] + my_raw * TR[2][1] + mz_raw * TR[2][2]
-        return(mx,my,mz)
+        return(mx, my, mz)
 
     def Calibrate(self):
         print("Calibrating. Press switch when done.")
@@ -97,21 +79,9 @@ class TCCompass:
             pyb.delay(1000)
         print(xa/100, ya/100, za/100)
 
-    # def view(self):
-    #     while True:
-    #         self.update()
-    #         print("{0}    {1}".format(self.fuse.heading, self.fuseh.heading))
-
     @property
     def heading(self):
         a = self.hrp[0]
-        if a < 0:
-            a += 360
-        return a
-
-    @property
-    def headingh(self):
-        a = self.fuseh.heading()
         if a < 0:
             a += 360
         return a
@@ -126,133 +96,6 @@ class TCCompass:
 
     @property
     def output(self):
-
-        outstring = [CMP("1,{},{}".format(self.mag, self.hrp[0])).msg,
-                     CMP("2,{},{}".format(self.magh, self.fuseh.heading)).msg,
-                     HDG(self.hrp[0]).msg]
-        for x in range(0,2):
-            outstring[x] = outstring[x].replace('(','')
-            outstring[x] = outstring[x].replace(')','')
-
-        return outstring
-
-class TCCompass_noLSM:
-
-    def __init__(self, imu, hmc_side=1, timeout=1000):
-        # load configuration file
-        with open('calibration.conf', mode='r') as f:
-            mpu_conf = f.readline()
-            lsm_conf = f.readline()
-            mcnf = pickle.loads(mpu_conf)
-            #lcnf = pickle.loads(lsm_conf)
-        self.MPU_Centre = mcnf[0][0]
-        self.MPU_TR = mcnf[1]
-        #self.LSM_Centre = lcnf[0][0]
-        #self.LSM_TR = lcnf[1]
-        self.counter = pyb.millis()
-        self.timeout = timeout
-
-        # setup compasses
-
-        # MPU9250
-        self.imu = imu
-
-        # LSM303
-        #self.lsm = LSM303.TiltCompCompass(hmc_side)
-        #self.lsm.setDeclination(0)
-        #self.lsm.setContinuousMode()
-
-        self.gyrobias = (-1.394046, 1.743511, 0.4735878)
-
-        # setup fusions
-        self.fuse = Fusion()
-        #self.fuseh = Fusion()#TODO: temp for comparison
-        self.update()
-        self.hrp = [self.fuse.heading, self.fuse.roll, self.fuse.pitch]
-
-    def process(self):
-        self.update()
-        if pyb.elapsed_millis(self.counter) >= self.timeout:
-            self.hrp = [self.fuse.heading, self.fuse.roll, self.fuse.pitch]
-            self.counter = pyb.millis()
-            return True
-        return False
-
-    def update(self):
-        accel = self.imu.accel.xyz
-        gyroraw = self.imu.gyro.xyz
-
-        gyro = [gyroraw[0] - self.gyrobias[0], gyroraw[1] - self.gyrobias[1], gyroraw[2] - self.gyrobias[2]]
-
-        self.mag = self.imu.mag.xyz
-        #self.magh = self.lsm.getAxes()
-
-
-        self.fuse.update(accel, gyro, self.adjust_mag(self.mag, self.MPU_Centre, self.MPU_TR))
-        #self.fuseh.update(accel, gyro, self.adjust_mag(self.magh, self.LSM_Centre, self.LSM_TR))
-
-    def getmag(self):
-        return self.imu.mag.xyz
-
-    @staticmethod
-    def adjust_mag(mag, centre, TR):
-        mx_raw = mag[0] - centre[0]
-        my_raw = mag[1] - centre[1]
-        mz_raw = mag[2] - centre[2]
-
-        mx = mx_raw * TR[0][0] + my_raw * TR[0][1] + mz_raw * TR[0][2]
-        my = mx_raw * TR[1][0] + my_raw * TR[1][1] + mz_raw * TR[1][2]
-        mz = mx_raw * TR[2][0] + my_raw * TR[2][1] + mz_raw * TR[2][2]
-        return(mx,my,mz)
-
-    def Calibrate(self):
-        print("Calibrating. Press switch when done.")
-        sw = pyb.Switch()
-        self.fuse.calibrate(self.getmag, sw, lambda: pyb.delay(100))
-        print(self.fuse.magbias)
-
-    def gyrocal(self):
-        xa = 0
-        ya = 0
-        za = 0
-        for x in range(0, 100):
-            xyz = self.imu.gyro.xyz
-            xa += xyz[0]
-            ya += xyz[1]
-            za += xyz[2]
-            pyb.delay(1000)
-        print(xa/100, ya/100, za/100)
-
-    # def view(self):
-    #     while True:
-    #         self.update()
-    #         print("{0}    {1}".format(self.fuse.heading, self.fuseh.heading))
-
-    @property
-    def heading(self):
-        a = self.hrp[0]
-        if a < 0:
-            a += 360
-        return a
-
-    @property
-    def headingh(self):
-        #a = self.fuseh.heading()
-        if a < 0:
-            a += 360
-        return a
-
-    @property
-    def roll(self):
-        return self.hrp[1]
-
-    @property
-    def pitch(self):
-        return self.hrp[2]
-
-    @property
-    def output(self):
-
         outstring = [CMP("1,{},{}".format(self.mag, self.hrp[0])).msg,
                      HDG(self.hrp[0]).msg, XDR(self.hrp[2],self.hrp[1])]
         for x in range(0,2):
@@ -261,156 +104,12 @@ class TCCompass_noLSM:
 
         return outstring
 
-class TCCompass_LSMADJUST:
-
-    def __init__(self, imu, hmc_side=1, timeout=1000):
-        # load configuration file
-        with open('calibration.conf', mode='r') as f:
-            mpu_conf = f.readline()
-            lsm_conf = f.readline()
-            mcnf = pickle.loads(mpu_conf)
-            lcnf = pickle.loads(lsm_conf)
-        self.MPU_Centre = mcnf[0][0]
-        self.MPU_TR = mcnf[1]
-        self.LSM_Centre = lcnf[0][0]
-        self.LSM_TR = lcnf[1]
-        self.counter = pyb.millis()
-        self.timeout = timeout
-
-        # setup compasses
-
-        # MPU9250
-        self.imu = imu
-
-        # LSM303
-        self.lsm = LSM303.TiltCompCompass(hmc_side)
-        self.lsm.setDeclination(0)
-        self.lsm.setContinuousMode()
-
-        self.gyrobias = (-1.394046, 1.743511, 0.4735878)
-
-        # setup fusions
-        self.fuse = Fusion()
-        self.fuseh = Fusion()#TODO: temp for comparison
-        self.update()
-        self.hrp = [self.fuse.heading, self.fuse.roll, self.fuse.pitch]
-
-    def process(self):
-        self.update()
-        if pyb.elapsed_millis(self.counter) >= self.timeout:
-            self.hrp = [self.fuse.heading, self.fuse.roll, self.fuse.pitch]
-            self.counter = pyb.millis()
-            return True
-        return False
-
-
-    def update(self):
-        accel = self.imu.accel.xyz
-        gyroraw = self.imu.gyro.xyz
-
-        gyro = [gyroraw[0] - self.gyrobias[0], gyroraw[1] - self.gyrobias[1], gyroraw[2] - self.gyrobias[2]]
-
-        self.mag = self.imu.mag.xyz
-        self.magh = self.lsm.getAxes()
-        valid_lsm = False
-        while not valid_lsm:
-            valid_lsm = self.check_lsm()
-
-        self.fuse.update(accel, gyro, self.adjust_mag(self.mag, self.MPU_Centre, self.MPU_TR))
-        self.fuseh.update(accel, gyro, self.adjust_mag(self.magh, self.LSM_Centre, self.LSM_TR))
-
-    def getmag(self):
-        return self.imu.mag.xyz
-
-    def check_lsm(self):
-        for a in self.magh:
-            if a is None:
-                self.adjust_lsm()
-                return False
-        return True
-
-    def adjust_lsm(self):
-        g=[0.88, 1.3, 1.9, 2.5, 4.0, 4.7, 5.6, 8.1]
-        if self.lsm.scale_reg < 7:
-            gauss=g[self.lsm.scale_reg+1]
-            self.lsm.setMagScale(gauss)
-
-
-    @staticmethod
-    def adjust_mag(mag, centre, TR):
-        mx_raw = mag[0] - centre[0]
-        my_raw = mag[1] - centre[1]
-        mz_raw = mag[2] - centre[2]
-
-        mx = mx_raw * TR[0][0] + my_raw * TR[0][1] + mz_raw * TR[0][2]
-        my = mx_raw * TR[1][0] + my_raw * TR[1][1] + mz_raw * TR[1][2]
-        mz = mx_raw * TR[2][0] + my_raw * TR[2][1] + mz_raw * TR[2][2]
-        return(mx,my,mz)
-
-    def Calibrate(self):
-        print("Calibrating. Press switch when done.")
-        sw = pyb.Switch()
-        self.fuse.calibrate(self.getmag, sw, lambda: pyb.delay(100))
-        print(self.fuse.magbias)
-
-    def gyrocal(self):
-        xa = 0
-        ya = 0
-        za = 0
-        for x in range(0, 100):
-            xyz = self.imu.gyro.xyz
-            xa += xyz[0]
-            ya += xyz[1]
-            za += xyz[2]
-            pyb.delay(1000)
-        print(xa/100, ya/100, za/100)
-
-    # def view(self):
-    #     while True:
-    #         self.update()
-    #         print("{0}    {1}".format(self.fuse.heading, self.fuseh.heading))
-
-    @property
-    def heading(self):
-        a = self.hrp[0]
-        if a < 0:
-            a += 360
-        return a
-
-    @property
-    def headingh(self):
-        a = self.fuseh.heading()
-        if a < 0:
-            a += 360
-        return a
-
-    @property
-    def roll(self):
-        return self.hrp[1]
-
-    @property
-    def pitch(self):
-        return self.hrp[2]
-
-    @property
-    def output(self):
-
-        outstring = [CMP("1,{},{}".format(self.mag, self.hrp[0])).msg,
-                     CMP("2,{},{}".format(self.magh, self.fuseh.heading)).msg,
-                     HDG(self.hrp[0]).msg]
-        for x in range(0,2):
-            outstring[x] = outstring[x].replace('(','')
-            outstring[x] = outstring[x].replace(')','')
-
-        return outstring
-
-
 def cthread(out_buf):
-    imu = MPU9250('X', transposition=(1,0,2), scaling=(1,-1,1))
+    imu = MPU9250('X', transposition=(0,1,2), scaling=(1,-1,1))
     global i2c_object
     i2c_object = imu._mpu_i2c
     yield 0.03                                  # Allow accelerometer to settle
-    compass = TCCompass_LSMADJUST(imu, timeout=1000)
+    compass = TCCompass(imu, timeout=1000)
 
     while True:
         yield
--- jambo.py
+++ jambo.py
@@ -4,10 +4,8 @@
 # import functionality
 from usched import Sched # cooperative Scheduling
 import compass # tiltcompensated compass
-from nasacontrol import NASADepthThread, NASAWindThread
 from Seatalk import seatalkthread
 from barometer import barometerthread
-#import pickle
 import upower
 import pyb
 import time
@@ -15,61 +13,31 @@ from nmeagenerator import ERR
 
 
 def run():
-    # setup uart object to pass
-  #  output = pyb.UART(1, 4800)
-    # generate objects
     OB = output_buffer()
     compassthread = compass.cthread(OB)
-#    mpu_i2c = compass.i2c_object
     pyb.delay(100)
-    othread = outputthread(OB)#, test=True)
-    objSched=Sched()
+    othread = outputthread(OB)
+    objSched = Sched()
     objSched.add_thread(seatalkthread(OB))
     objSched.add_thread(compassthread)
-#    objSched.add_thread(NASAWindThread(OB))
-#    objSched.add_thread(NASADepthThread(OB))
     pyb.delay(100)
-    objSched.add_thread(barometerthread(OB))#i2c_object=mpu_i2c))
+    objSched.add_thread(barometerthread(OB))
     objSched.add_thread(othread)
     objSched.run()
 
-def test():
-    # setup uart object to pass
-  #  output = pyb.UART(1, 4800)
-    # generate objects
-    OB = output_buffer()
-    compassthread = compass.cthread(OB)
-#    mpu_i2c = compass.i2c_object
-    pyb.delay(100)
-    othread = outputthread(OB)#, test=True)
-    objSched=Sched()
-#    objSched.add_thread(seatalkthread(OB))
-    objSched.add_thread(compassthread)
-#    objSched.add_thread(NASAWindThread(OB))
-#    objSched.add_thread(NASADepthThread(OB))
-#    pyb.delay(100)
-#    objSched.add_thread(barometerthread(OB))#i2c_object=mpu_i2c))
-    objSched.add_thread(othread)
-    objSched.run()
 
-def outputthread(outbuf, test = False):
+def outputthread(outbuf, test=False):
     if test:
-        outputserial= None
+        outputserial = None
     else:
-        outputserial=pyb.UART(6, 115200)
+        outputserial = pyb.UART(6, 115200)
 
     if upower.vbat() < 2.0:
         outbuf.write(ERR('Low Backup Battery').msg)
     while True:
-        yield 0.1 #TODO reset
+        yield 0.1
         outbuf.print(outputserial)
-#        outbuf.clear()
-        # if test:
-        #
-        #     print(''.join(outbuf))
-        # else:
-        #     outputserial.write(''.join(outbuf))
-        # outbuf=[]
+
 
 class output_buffer:
 
@@ -82,9 +50,9 @@ class output_buffer:
         self.buf.extend(data)
 
     def print(self, uart):
-        if len(self.buf)>0:
+        if len(self.buf) > 0:
             output = ''.join(self.buf)
-            self.buf=[]
+            self.buf = []
             if uart == None:
                 print(output)
             else:
@@ -103,58 +71,59 @@ class output_buffer:
                 # distance - log distance for day
     def logupdate(self, log):
         return
-	    self.log['alive'] = log
-        self.save()
+#        self.log['alive'] = log
+#        self.save()
 
     def save(self):
-	    return
-        bkpram = upower.BkpRAM()
-        z = pickle.dumps(self.log).encode('utf8')
-        bkpram[0] = len(z)
-        bkpram.ba[4: 4+len(z)] = z # Copy into backup RAM
+        return
+#        bkpram = upower.BkpRAM()
+#        z = pickle.dumps(self.log).encode('utf8')
+#        bkpram[0] = len(z)
+#        bkpram.ba[4: 4+len(z)] = z # Copy into backup RAM
 
     def load(self):
-	    return
-        reason = upower.why()                           # Why have we woken?
-        if reason == 'BOOT':                            # first boot
-            self.log = log_generator()
-            self.save()
-        elif reason == 'POWERUP':
-            bkpram = upower.BkpRAM()
-            self.log = pickle.loads(bytes(bkpram.ba[4:4+bkpram[0]]).decode('utf-8')) # retrieve dictionary
-            if len(self.log) == 0:
-                self.log = log_generator()
-        currentdaydata = time.time()
-        currentdaytuple = time.localtime(currentdaydata)
+        return
+#        reason = upower.why()                           # Why have we woken?
+#        if reason == 'BOOT':                            # first boot
+#            self.log = log_generator()
+#            self.save()
+#        elif reason == 'POWERUP':
+#            bkpram = upower.BkpRAM()
+#            self.log = pickle.loads(bytes(bkpram.ba[4:4+bkpram[0]]).decode('utf-8')) # retrieve dictionary
+#            if len(self.log) == 0:
+#                self.log = log_generator()
+#        currentdaydata = time.time()
+#        currentdaytuple = time.localtime(currentdaydata)
         # adjust currentdaytuple to make to midnight on day of interest
-        cdt = (currentdaytuple[0], currentdaytuple[1], currentdaytuple[2], 0, 0, 0,
-               currentdaytuple[6],currentdaytuple[7])
+#        cdt = (currentdaytuple[0], currentdaytuple[1], currentdaytuple[2], 0, 0, 0,
+#               currentdaytuple[6],currentdaytuple[7])
 
-        date = time.mktime(cdt)
+#        date = time.mktime(cdt)
 
-        dt = self.log['date']
-        dist = self.log['daily'] + self.log['alive']
-        if date == dt:
-            self.log['daily'] = dist
-            self.log['alive'] = 0
+#        dt = self.log['date']
+#        dist = self.log['daily'] + self.log['alive']
+#        if date == dt:
+#            self.log['daily'] = dist
+#            self.log['alive'] = 0
 
         # if new day adjust all records and create history
-        elif date > dt and dist > 0:
-            hist = {'date':dt, 'distance':dist}
-            self.log['history'].insert(0,hist)
-            self.log['history'] = self.log['history'][:-1]
-            self.log['total'] = self.log['total'] + dist
+#        elif date > dt and dist > 0:
+#            hist = {'date' : dt, 'distance':dist}
+#            self.log['history'].insert(0,hist)
+#            self.log['history'] = self.log['history'][:-1]
+#            self.log['total'] = self.log['total'] + dist
 
-            self.log['daily'] = 0
-            self.log['alive'] = 0
-            self.log['date'] = date
+#            self.log['daily'] = 0
+#            self.log['alive'] = 0
+#            self.log['date'] = date
+
 
 def log_generator():
     history = []
-    loghistory = {'date':0, 'distance':0}
-    for x in range(0,30):
+    loghistory = {'date': 0, 'distance': 0}
+    for x in range(0, 30):
         history.append(loghistory)
-    return {'total': 0, 'daily': 0, 'date':0, 'alive':0, 'history':history}
+    return {'total': 0, 'daily': 0, 'date': 0, 'alive': 0, 'history': history}
 
 def setup():
     dt = pyb.RTC().datetime()
@@ -166,9 +135,10 @@ def setup():
             inputint("Day of week [1 = Monday]: ", 1, 7),
             inputint("Hour [0..23]: ", 0, 23),
             inputint("Minute [0..59]: ", 0, 59),
-            0,0))
+            0, 0))
         dt = pyb.RTC().datetime()
-    print('RTC: {:04},{:02},{:02} {:02}:{:02}'.format(dt[0],dt[1],dt[2],dt[4],dt[5]))
+    print('RTC: {:04},{:02},{:02} {:02}:{:02}'.format(dt[0], dt[1], dt[2], dt[4], dt[5]))
+
 
 def inputint(pr, mn, mx):
     x = input(pr)
@@ -181,4 +151,3 @@ def inputint(pr, mn, mx):
     if (x > mx):
         x = mx
     return x
-
